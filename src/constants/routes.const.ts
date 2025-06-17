5b196bd2c411256cf93152f1357865242b84ca2b
keren-elbaz-m <kerene@moveo.co.il>
2025-06-17 18:34:12 +0300

    Cloudinary configuration
    Integrated Cloudinary image upload with Multer and upload endpoint.
    Created uploadImage utility to handle Cloudinary uploads.
    Set up /api/upload/image route with Multer middleware

--- src/constants/routes.const.ts
+++ src/constants/routes.const.ts
@@ -4,6 +4,7 @@ export const ROUTES = {
     ROOT: RoutePath.ROOT,
     API: RoutePath.API,
     HEALTH: RoutePath.HEALTH,
+    UPLOAD: RoutePath.UPLOAD,
     RESTAURANTS: RoutePath.RESTAURANTS,
     CHEF: RoutePath.CHEF,
 };
--- src/routes/api.router.ts
+++ src/routes/api.router.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import restaurantRoutes from "../modules/restaurant/restaurant.route";
 import chefRoutes from "../modules/Chef/chef.route";
+import uploadRoutes from "./upload.routes";
 
 import { ROUTES } from "../constants/routes.const";
 
@@ -9,4 +10,5 @@ const apiRouter = express.Router();
 apiRouter.use(ROUTES.RESTAURANTS, restaurantRoutes);
 apiRouter.use(ROUTES.CHEF, chefRoutes);
 
+apiRouter.use(ROUTES.UPLOAD, uploadRoutes);
 export default apiRouter;
